2eac910f04686dbc84530e3c0ffdc90548b4c8a9
Hugo <Hugo.thiebaudet@gmail.com>
2023-09-08 13:25:58 +0200

    MAJ

--- TP1/prog1.py
+++ TP1/prog1.py
@@ -1,5 +1,6 @@
-while (True):
-print ('Veuillez entrez un nombre :')
+	
+while True:	
+	print ('Veuillez entrez un nombre :')
 	nb = input()
 	nb2 = int(nb)
-print ('Le produit au carré de', nb, 'est égal à : ', nb2**2 )
+	print ('Le produit au carré de', nb, 'est égal à : ', nb2**2 )
