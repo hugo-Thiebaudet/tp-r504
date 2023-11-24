3febf4e545431a4a5ce0fcf31e319724855b33af
hugo-Thiebaudet <hugo.thiebaudet@gmail.com>
2023-11-24 09:46:56 +0100

    maj

---
+++ TP4/run_mysql_old.sh
@@ -0,0 +1,9 @@
+# lancement serveur sql
+docker run --rm -d \
+        -p 3307:3306 \
+        -v vol-sql-demo:/var/lib/mysql \
+        --name tp4-sql \
+        --env MYSQL\_ROOT\_Password=foo \
+        --network net-tp4 \
+        mysql
+
