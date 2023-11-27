ea63fef80f7d7b37f7fee60e9f1efa0fdc437a64
hugo-Thiebaudet <hugo.thiebaudet@gmail.com>
2023-11-27 14:27:03 +0100

    maj

--- tp9/ClientTCP1.java
+++ tp9/ClientTCP1.java
@@ -2,14 +2,11 @@ import java.io.*;
 import java.net.*;
 
 public class ClientTCP1 {
-    public static void main(String[] args) { // Ajout des arguments de la méthode main
+    public static void main(String[] args) { 
         try {
-            Socket socket = new Socket("localhost", 2016);
-            
-            // Correction de la casse de la classe DataOutputStream
-            DataOutputStream dOut = new DataOutputStream(socket.getOutputStream());
-            
-            dOut.writeUTF("message test");
+            Socket socket = new Socket("localhost", 2016);    
+            DataOutputStream dOut = new DataOutputStream(socket.getOutputStream());      
+            dOut.writeUTF("bip bip");
             
             socket.close();
         } catch (Exception ex) {
