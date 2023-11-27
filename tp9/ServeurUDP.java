1a95c3b990ceccd4eb2189495e2855aa77c5a6a6
hugo-Thiebaudet <hugo.thiebaudet@gmail.com>
2023-11-27 13:21:21 +0100

    maj

--- tp9/ServeurUDP.java
+++ tp9/ServeurUDP.java
@@ -1,21 +1,21 @@
 import java.io.*;
 import java.net.*;
 
-public class ServeurUDP
-{
-	public static void main()
-	{
-
-		DatagramSocket sock = new DatagramSocket(1234);
-		while(true)
-		{
-			System.out.println( "-waiting data" );
-			DatagramPacket packet = new DatagramPacket(new byte[1024], 1024);
-			sock.receive(packet);
-			String str = new String(packet.getData() );
-			System.out.println( "str=" + str );
-		}
-	}
+public class ServeurUDP {
+    public static void main(String[] args) { // Ajout des arguments de la méthode main
+        try {
+            DatagramSocket sock = new DatagramSocket(1234);
+            while (true) {
+                System.out.println("-waiting data");
+                DatagramPacket packet = new DatagramPacket(new byte[1024], 1024);
+                sock.receive(packet);
+                String str = new String(packet.getData() );
+                System.out.println("str=" + str);
+            }
+        } catch (Exception ex) {
+            System.out.println("erreur !");
+            ex.printStackTrace();
+        }
+    }
 }
-
 
