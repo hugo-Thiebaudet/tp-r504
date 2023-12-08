c5efc6abc223255ad5ffaca2a2939271b3b1b311
hugo-Thiebaudet <hugo.thiebaudet@gmail.com>
2023-12-08 08:54:04 +0100

    maj

---
+++ sae/conf.php
@@ -0,0 +1,6 @@
+$dolibarr_main_db_host = 'db'; // Assurez-vous que c'est le nom du service du conteneur de base de données.
+$dolibarr_main_db_port = '3306'; // Le port par défaut du serveur MySQL.
+$dolibarr_main_db_name = 'dolibarr'; // Le nom de la base de données.
+$dolibarr_main_db_user = 'dolibarr'; // L'utilisateur de la base de données.
+$dolibarr_main_db_pass = 'dolibarrpass'; // Le mot de passe de la base de données.
+
---
+++ tp10/Client.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import org.apache.http.HttpEntity;
+import org.apache.http.client.*;
+import org.apache.http.client.methods.*;
+import org.apache.http.impl.client.*;
+
+public class Client {
+    public static void main(String[] args ) {
+
+            System.out.println(":)");
+
+	}
+}
